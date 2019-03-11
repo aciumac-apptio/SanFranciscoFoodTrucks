4b6f2bb3d63ef6e83df10b9baae4cc42ee250edd
Artem Chumak <artem_chumak@hotmail.com>
2019-03-11 15:50:14 -0700

    Add project files.

---
+++ FoodTrucks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodTrucks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
